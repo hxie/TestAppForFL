142cbf09a50e125d2234961b8cfae22c41083260
Hale Xie <hale.xie@helplightning.com>
2018-01-09 12:45:16 +0800

    Initial check-in

---
+++ TestAppForFL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAppForFL
+//
+//  Created by Hale Xie on 09/01/2018.
+//  Copyright © 2018 hxie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestAppForFL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAppForFL
+//
+//  Created by Hale Xie on 09/01/2018.
+//  Copyright © 2018 hxie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
